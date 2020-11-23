2246c4d9cacec19b041777d24975b931fb87edef
Željko Milošević <zeljkom96@gmail.com>
2020-11-23 17:54:36 +0100

    Initial commit! 🚀🔥

---
+++ bitcoin-service/src/test/java/com/ws/sep/bitcoinservice/BitcoinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.sep.bitcoinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitcoinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
