f262f45c173bc360925424b3650d9fd01aac7212
Željko Milošević <zeljkom96@gmail.com>
2020-12-15 22:10:05 +0100

    blank application pcc, issuer, acquirer

---
+++ issuer/src/test/java/com/ws/sep/issuer/IssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.sep.issuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
